96436500693551c524de274b1e241099f07251a4
Kapil Singh <singh.kapy@gmail.com>
2017-10-06 20:15:40 +0200

    Added some icons and more styling

--- components/RadioFooter.js
+++ components/RadioFooter.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {Left, Right} from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class RadioFooter extends Component {
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <Text style={styles.timeLine}>TimeLine</Text>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <Text style={{flex: 3, alignItems: 'flex-start'}}>0:01</Text>
-          <Text style={{alignItems: 'flex-end'}}>-2:49</Text>
+        <Text style={styles.statusBar}>Status Bar</Text>
+        <View style={styles.statusBarTime}>
+          <Left>
+            <Text>0:01</Text>
+          </Left>
+          <Right>
+            <Text>-2:49</Text>
+          </Right>
         </View>
         <Text style={{textAlign: 'center'}}>TrackPlaying</Text>
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-end'}}>
-          <Text style={{flex: 3, alignItems: 'flex-start'}}>Play</Text>
-          <Text style={{alignItems: 'flex-end'}}>Volume</Text>
+        <View style={styles.playerControls}>
+          <Left>
+            <TouchableOpacity style={styles.playStop}>
+              <Icon name="play" size={40}/>
+            </TouchableOpacity>
+          </Left>
+          <Right>
+            <TouchableOpacity style={styles.volume}>
+              <Icon name="volume-up" size={40}/>
+            </TouchableOpacity>
+          </Right>
         </View>
       </View>
     );
@@ -21,8 +35,27 @@ class RadioFooter extends Component {
 }
 
 const styles = {
-  timeLine: {
+  statusBar: {
     textAlign: 'center'
+  },
+  statusBarTime: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  playerControls: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  playStop: {
+    alignItems: 'flex-start',
+    padding: 20,
+    paddingBottom: 10
+  },
+  volume: {
+    alignItems: 'flex-end',
+    padding: 20,
+    paddingBottom: 10
   }
 };
 
