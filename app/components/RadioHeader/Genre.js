38d01c9405440777dac77c8c1340ccc2ac463b68
Kapil Singh <singh.kapy@gmail.com>
2017-10-11 20:54:51 +0200

    added swiper to change genre

--- app/components/RadioHeader/Genre.js
+++ app/components/RadioHeader/Genre.js
@@ -1,83 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {
-  View,
   Text,
-  Image,
-  TouchableOpacity
+  View,
+  Image
 } from 'react-native';
 import {
-  Left,
-  Body,
-  Right
+  Body
 } from 'native-base';
-import {Arrow} from './Arrow';
-import * as Animatable from 'react-native-animatable';
-import {genres} from '../../globals';
-
-const GENRES = ['ROCK', 'PARTY', 'POP', 'CLASSIC'];
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Swiper from 'react-native-swiper'
+import {genres} from "../../globals";
 
 class Genre extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      direction: '',
-      genreIndex: 0,
-      genre: Object.keys(genres[0])[0]
-    };
 
-    this._setDirection = this._setDirection.bind(this);
-  }
-
-  _setDirection(direction) {
-    this.setState({
-      direction: direction,
-      genreIndex: this._getGenreIndex(direction)
+  _returnThumbnails() {
+    return genres.map(genre => {
+      return <View key={genre} style={styles.genre}>
+        <Body>
+        <Image
+          key={Object.values(genre)[0]}
+          style={{height: 40, width: 40}}
+          source={{uri: Object.values(genre)[0]}}
+        />
+        </Body>
+        <Text
+          key={Object.keys(genre)[0]}
+          style={styles.genreText}>
+          {Object.keys(genre)[0]}
+        </Text>
+      </View>
     });
-    direction == 'left' ? this.refs.view.fadeInLeft(500) : this.refs.view.fadeInRight(500);
-  }
-
-  _getGenreIndex(direction) {
-    let genreIndex = this.state.genreIndex;
-
-    if (direction === 'left') {
-      if (genreIndex > 0) {
-        genreIndex = genreIndex - 1;
-      } else {
-        genreIndex = genres.length - 1;
-      }
-    } else if (direction === 'right') {
-      if (genreIndex < genres.length - 1) {
-        genreIndex = genreIndex + 1;
-      } else {
-        genreIndex = 0;
-      }
-    }
-    return genreIndex;
   }
 
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={styles.genre}>
-          <Left>
-            <Arrow iconName="chevron-left" direction='left' setDirection={this._setDirection}/>
-          </Left>
-          <Body>
-            <Animatable.Text ref="view" style={{fontSize: 18, color: '#393939'}}>
-              {Object.keys(genres[this.state.genreIndex])[0]}
-            </Animatable.Text>
-          </Body>
-          <Right>
-            <Arrow iconName="chevron-right" direction='right' setDirection={this._setDirection}/>
-          </Right>
-        </View>
-        <Body>
-        {/*<Animatable.Image*/}
-          {/*ref="view"*/}
-          {/*style={{height: 50, width: 50}}*/}
-          {/*source={{uri: Object.values(genres[this.state.genreIndex])[0]}}*/}
-        {/*/>*/}
-        </Body>
+        <Swiper showsButtons
+                prevButton={<Icon name="chevron-left" color='#393939' size={28}/>}
+                nextButton={<Icon name="chevron-right" color='#393939' size={28}/>}
+                showsPagination={false}>
+          {this._returnThumbnails()}
+        </Swiper>
       </View>
     );
   }
@@ -86,11 +49,15 @@ class Genre extends Component {
 const styles = {
   genre: {
     flex: 1,
-    flexDirection: 'row',
-    paddingTop: 24,
-    paddingBottom: 8,
-    paddingLeft: 5,
-    paddingRight: 5
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 30
+  },
+  genreText: {
+    fontSize: 16,
+    color: '#393939',
+    textAlign: 'center',
+    paddingBottom: 10
   }
 };
 
